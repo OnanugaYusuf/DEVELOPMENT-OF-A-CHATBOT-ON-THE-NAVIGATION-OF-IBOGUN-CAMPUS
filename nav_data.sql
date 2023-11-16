15eaa6490c6330ca338474de2df5b8412fca9d2c
Adebanjo Olajide Opeyemi <131617858+its-jedu@users.noreply.github.com>
2023-11-16 15:08:57 +0100

    Add files via upload

---
+++ nav_data.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM navbot.nav_data
+LIMIT 0, 1000
+
+-- Date: 2023-10-31 18:04
+*/
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (1,'School Gate',6.817359,3.106116,'Computer Department',209.21,160);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (2,'School Gate',6.817359,3.106116,'Agric Department',241.40,178);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (3,'School Gate',6.817359,3.106116,'Electrical Department',273.59,204);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (4,'School Gate',6.817359,3.106116,'Mechanical Department',289.68,238);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (5,'School Gate',6.817359,3.106116,'Civil and Architecture Department',450.62,365);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (6,'School Gate',6.817359,3.106116,'Fine Arts Workshop',692.91,540);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (7,'School Gate',6.817359,3.106116,'Library',1046.20,840);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (8,'School Gate',6.817359,3.106116,'Mechanical Workshop',96.56,68);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (9,'School Gate',6.817359,3.106116,'Motion Ground',193.12,150);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (10,'School Gate',6.817359,3.106116,'Clinic',193.00,150);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (11,'School Gate',6.817359,3.106116,'Mosque',241.40,185);
+INSERT INTO `` (`id`,`starting_location`,`starting_latitude`,`starting_longitude`,`destination_name`,`distance_meter`,`time_seconds`) VALUES (12,'School Gate',6.817359,3.106116,'Fine Arts Department',177.00,141);
